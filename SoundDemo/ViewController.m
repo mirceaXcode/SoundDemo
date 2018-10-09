463ad09b928bcc21e3eb71b72ef26872580feed1
Mircea Popescu <mirceap@gmail.com>
2018-10-08 21:13:51 -0500

    Added more sounds for the Drum Kit Assignment

--- SoundDemo/ViewController.m
+++ SoundDemo/ViewController.m
@@ -14,6 +14,8 @@
 
 @property (assign, nonatomic) SystemSoundID beepA;
 @property (assign, nonatomic) SystemSoundID beepB;
+@property (assign, nonatomic) SystemSoundID beepC;
+@property (assign, nonatomic) SystemSoundID beepD;
 
 @property (nonatomic,strong) AVAudioPlayer *player;
 
@@ -27,12 +29,12 @@
 // Starting iOS 10 we have to use the AudioServicesPlaySystemSoundWithCompletion function, which includes the autodistruction of the sound right after usage. Because of that, we don't need to use a dealloc funtion anymore, but we have to construc the sound every time we push the button, because it gets destroyed after each usage!!!
     
     // Load the media file
-    NSURL *urlC = [[NSBundle mainBundle] URLForResource:@"Parazitii.mp3" withExtension:nil];
+    NSURL *urlSong = [[NSBundle mainBundle] URLForResource:@"Parazitii.mp3" withExtension:nil];
     
     NSError *err;
     
     // Setup AVAudioPlayer
-    self.player = [[AVAudioPlayer alloc] initWithContentsOfURL:urlC error:&err];
+    self.player = [[AVAudioPlayer alloc] initWithContentsOfURL:urlSong error:&err];
     
     if(!self.player){
         UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Could't load mp3" message:[err localizedDescription] preferredStyle:UIAlertControllerStyleAlert];
@@ -49,7 +51,7 @@
     // Casting -> Don't generate ARC meta data
     
     // Load the sound / create the sound ID
-    NSURL *urlA = [[NSBundle mainBundle] URLForResource:@"marimba.aif" withExtension:nil];
+    NSURL *urlA = [[NSBundle mainBundle] URLForResource:@"marimba.aiff" withExtension:nil];
     OSStatus statusReport = AudioServicesCreateSystemSoundID((__bridge CFURLRef)urlA, &_beepA);
     
     // Play the sound A
@@ -67,14 +69,9 @@
 }
 
 - (IBAction)playSoundB:(id)sender {
-    
-    // Archaic C code
-    // __bridge = C-level cast
-    // Tels ARC to stop taking notice of the casted object
-    // Casting -> Don't generate ARC meta data
-    
+
     // Load the sound / create the sound ID
-    NSURL *urlB = [[NSBundle mainBundle] URLForResource:@"sncf.aif" withExtension:nil];
+    NSURL *urlB = [[NSBundle mainBundle] URLForResource:@"sncf.aiff" withExtension:nil];
     OSStatus statusReport = AudioServicesCreateSystemSoundID((__bridge CFURLRef)urlB, &_beepB);
     
     // Play the sound B
@@ -90,6 +87,45 @@
     }
 }
 
+- (IBAction)playSoundC:(id)sender {
+    
+    // Load the sound / create the sound ID
+    NSURL *urlC = [[NSBundle mainBundle] URLForResource:@"bike horn.aiff" withExtension:nil];
+    OSStatus statusReport = AudioServicesCreateSystemSoundID((__bridge CFURLRef)urlC, &_beepC);
+    
+    // Play the sound C
+    if(statusReport == kAudioServicesNoError) {
+        AudioServicesPlaySystemSoundWithCompletion(_beepC, ^{
+            AudioServicesDisposeSystemSoundID(self.beepC);
+        });
+        
+    } else {
+        
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Couldn't load beepC" message:@"BeepC problem" preferredStyle:UIAlertControllerStyleAlert];
+        [self presentViewController:alert animated:YES completion:nil];
+    }
+    
+}
+
+- (IBAction)playSoundD:(id)sender {
+    
+    // Load the sound / create the sound ID
+    NSURL *urlD = [[NSBundle mainBundle] URLForResource:@"toy horn.aiff" withExtension:nil];
+    OSStatus statusReport = AudioServicesCreateSystemSoundID((__bridge CFURLRef)urlD, &_beepD);
+    
+    // Play the sound D
+    if(statusReport == kAudioServicesNoError) {
+        AudioServicesPlaySystemSoundWithCompletion(_beepD, ^{
+            AudioServicesDisposeSystemSoundID(self.beepD);
+        });
+        
+    } else {
+        
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Couldn't load beepD" message:@"BeepD problem" preferredStyle:UIAlertControllerStyleAlert];
+        [self presentViewController:alert animated:YES completion:nil];
+    }
+    
+}
 
 - (IBAction)playMedia:(id)sender {
     [self.player play];
